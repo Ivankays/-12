8fbfc3626388a8b30892b52e462214b36bd13765
Ivankays <144226977+Ivankays@users.noreply.github.com>
2023-12-11 13:07:38 +0300

    Add files via upload

---
+++ 2.pas
@@ -0,0 +1,21 @@
+﻿program zad2;
+var
+  N, K, i, j: integer;
+  outputFile: text;
+begin
+  write('Введите количество строк (N): ');
+  readln(N);
+  write('Введите длину строки (K): ');
+  readln(K);
+  assign(outputFile, 'stars.txt');
+  rewrite(outputFile);
+  for i := 1 to N do
+  begin
+    for j := 1 to K do
+      write(outputFile, '*');
+    writeln(outputFile);
+  end;
+  close(outputFile);
+  writeln('Файл успешно создан. Строки заполнены звездочками.');
+  writeln('Путь к файлу: ', GetCurrentDir, '\stars.txt');
+end.
